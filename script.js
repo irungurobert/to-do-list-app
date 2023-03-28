e9b47727b64b601ae44501a5a6f6e2c600e5abc2
irungurobert <robbieirungu82@gmail.com>
2023-03-28 11:18:30 +0300

    basic functions of a to do list app,including adding,edditting and deleting

---
+++ script.js
@@ -0,0 +1,52 @@
+//variables
+
+var addBtn = document.querySelector('.addBtn');
+var input = document.querySelector('.inputtask');
+var listcontainer = document.querySelector('.taskcontainer');
+
+//status
+
+const listStatus = 'no task';
+const activity = null;
+
+//events
+
+addBtn.addEventListener('click', function () {
+  let list = document.createElement('div');
+  list.classList.add('list');
+
+  let task = document.createElement('li');
+  task.classList.add('task');
+  task.innerHTML = input.value;
+
+  list.appendChild(task);
+
+  let checkBtn = document.createElement('button')
+  checkBtn.innerHTML = '<i class="fa fa-check"></i>'
+  checkBtn.classList.add('check');
+  list.appendChild(checkBtn);
+
+  let deleteBtn = document.createElement('button')
+  deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
+  deleteBtn.classList.add('delete');
+  list.appendChild(deleteBtn);
+
+
+  if (input.value === '') {
+    alert('enter task');
+  }
+  else {
+    listcontainer.appendChild(list);
+  }
+
+  input.value = '';
+
+  checkBtn.addEventListener('click',function(){
+   checkBtn.parentElement.style.textDecoration="underline";
+  });
+  
+
+  deleteBtn.addEventListener('click', function () {
+    deleteBtn.parentElement.remove();
+  })
+});
